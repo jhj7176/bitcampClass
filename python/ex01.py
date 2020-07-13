cf346bce43717e013640e43d4c19d07a52bc730c
jhj7176 <jhj7176@gmail.com>
2020-07-13 15:19:45 +0900

    css, .py

---
+++ python/ex01.py
@@ -0,0 +1,60 @@
+# print("hello")
+# print("hello")
+# print("hello")
+# print("hello")
+# print("hello")
+
+# a,b,c = 10, 20, 30
+
+# if a>b:
+#     print("c =",c)
+# else:
+#     print("b =",b)
+
+
+
+# # d = input()
+# # print("input 입력함수>>",d)
+
+# # a1,b2,c3 = input("출력문자 >> ").split(",")
+
+# # a,b = input("숫자형변환 >> ").split(",")
+# # #input은 String으로 리턴함
+# # print(int(a))
+# # print(int(b))# int로 캐스팅
+
+# # a,b=map(int, input().split("-"))
+
+# # print(a,b)
+
+# print(23, 44, sep=", ", end='.')
+
+# #print(서식문자열%(출력값1,출력값2...))
+# print(12,34,56)
+# print('gil','asdvadsf',sep='-')
+# print('help', end=' ')
+# print('me!!!')
+
+
+
+# print('%3d'%15)
+
+
+print('%3.3f'%12235.1234)
+print('%-3.3f'%12235.1234)
+print('%.3s%2.2f'%('help me',245.34534))
+print('%3s%2.2f'%('help me',245.34534))
+
+
+
+print('abc'.upper())
+print('asd'.capitalize())
+print('asdf eqe da'.title())
+print('sadewq'.replace('d','w'))
+print ('adsaaaafew'.count('a'))
+print('asdfsa'.find('f'))
+print('safewdaf'.index('w'))#0 1 2 3 4 5 ....
+
+array = [123, 'asd', 22.45]
+array.pop(1)
+
